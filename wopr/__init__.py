2f55e20039fb542140ffb7f2419acefa6d586561
plausibility <chris@gibsonsec.org>
2013-06-27 12:07:12 +1000

    Most of Phases 0 (LOGON), 1 (GAMES), and 2 (JOSHUA) are functional.

--- wopr/__init__.py
+++ wopr/__init__.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
 import cmd
 import sys
+import socket
 
 import gevent
 import gevent.server
 from gevent import sleep
 from telnetsrv.green import TelnetHandler, command
 
+import junk
+
+
+class Phases(object):
+    LOGON = 0
+    GAMES = 1
+    JOSHUA = 2
+    GLOBAL = 3
+
 
 class WOPRHandler(TelnetHandler):
     PROMPT = "LOGON:  "
     WELCOME = ""
+    session_start = lambda s: s.swrite(junk.CONNECTION, endln=False)
+
+    _phase = Phases.LOGON
 
     def _swrite(self, c, character=0.01, line=0.01):
         self.write(c)
@@ -22,17 +35,82 @@ class WOPRHandler(TelnetHandler):
         if endln:
             self._swrite("\n", line=0)
 
-    @command('help')
-    def help(self, params):
-        self.writeline("Yo: %s" % (params,))
+    @command(["exit", "quit", "bye"])
+    def _dont_leave(self, params):
+        pass
 
-    @command('joshua')
+    @command("logout")
+    def dont_leave_me(self, params):
+        self.swrite("--CONNECTION TERMINATED--")
+        self.RUNSHELL = False
+
+    # ---- PHASE 0 ----
+    @command(["000001", "falkens-maze", "armageddon"])
+    def terminate(self, params=None):
+        if self._phase != LOGON:
+            return
+
+        self.swrite(junk.CONNECTION_TERMINATED)
+        self.RUNSHELL = False
+
+    @command("help")
+    def help(self, params):
+        if self._phase != LOGON:
+            return
+
+        if len(params) == 0:
+            return self.terminate()
+        if params[0].lower() != "games":
+            return self.swrite(junk.HELP_LOGON)
+        self._phase = Phases.GAMES
+        return self.swrite(junk.HELP_GAMES)
+
+    # ---- PHASE 1 ----
+    @command("list")
+    def list(self, params):
+        if self._phase != Phases.GAMES:
+            return
+        if len(params) == 0:
+            return self.terminate()
+        if params[0].lower() == "games":
+            self._phase = Phases.JOSHUA
+            return self.swrite(junk.LIST_GAMES)
+
+    # ---- PHASE 2 ----
+    @command("joshua")
     def joshua(self, params):
-        self.swrite("GREETINGS PROFESSOR FALKEN.")
+        if self._phase != Phases.JOSHUA:
+            return
+
+        self.PROMPT = ""
+        self.swrite(junk.JOSHUA_LOGON_GARBAGE, character=0.002)
+        self.swrite(junk.JOSHUA_HELLO)
+
+    @command(["hello.", "im", "people", "love", "later."])
+    def phase2(self, params):
+        """ This sort of covers most of phase 2.
+            Not particularly clean, but hey, it works.
+        """
+        if self._phase != Phases.JOSHUA:
+            return
+
+        if self.history[-1].lower() == "hello.":
+            self.swrite(junk.JOSHUA_HOW_ARE_YOU)
+        elif self.history[-1].lower() == "im":
+            self.swrite(junk.JOSHUA_EXCELLENT)
+        elif self.history[-1].lower() == "people":
+            self.swrite(junk.JOSHUA_YES_THEY_DO)
+        elif self.history[-1].lower() == "love":
+            self.swrite(junk.JOSHUA_WOULDNT_YOU)
+        elif self.history[-1].lower() == "later.":
+            self._phase = Phases.GLOBAL
+            self.swrite(junk.JOSHUA_FINE)
+
+    # ---- PHASE 3 ----
+    def phase3(self, params):
+        pass
 
 if __name__ == "__main__":
     handler = WOPRHandler.streamserver_handle
     server = gevent.server.StreamServer(("", 8023), handler)
     server.serve_forever()
-    #swrite("GREETINGS PROFESSOR FALKEN.")
-    #swrite("HOW ARE YOU FEELING TODAY?")
---
+++ wopr/junk.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+""" This module simply contains a variety of strings used
+    by the WOPR to simulate the movie. Very boring, very
+    long, and _very_ repetitive. No point being here..
+"""
+
+CONNECTION = (
+    (" "*35 + "\n") * 3
+)
+
+# Phase 0
+CONNECTION_TERMINATED = (
+    "IDENTIFICATION NOT RECOGNISED BY SYSTEM\n"
+    "--CONNECTION TERMINATED--"
+)
+
+# Phase 0
+HELP_LOGON = (
+    "HELP NOT AVAILABLE"
+)
+
+# Phase 1
+HELP_GAMES = (
+    "'GAMES' REFERS TO MODELS, SIMULATIONS AND GAMES\n"
+    "WHICH HAVE TACTICAL AND STRATEGIC APPLICATIONS."
+)
+
+# Phase 1
+LIST_GAMES = (
+    "FALKEN'S MAZE\n"
+    "BLACK JACK\n"
+    "GIN RUMMY\n"
+    "HEARTS\n"
+    "BRIDGE\n"
+    "CHECKERS\n"
+    "CHESS\n"
+    "POKER\n"
+    "FIGHTER COMBAT\n"
+    "GUERRILLA ENGAGEMENT\n"
+    "DESERT WARFARE\n"
+    "AIR-TO-GROUND ACTIONS\n"
+    "THEATERWIDE TACTICAL WARFARE\n"
+    "THEATERWIDE BIOTOXIC AND CHEMICAL WARFARE\n"
+    "\n"
+    "GLOBAL THERMONUCLEAR WAR"
+)
+
+# Phase 2
+__n_50 = "\n" * 50
+JOSHUA_LOGON_GARBAGE = (
+    "#45 11456 11009 11893 11972 11315\n"
+    "PRT CON. 3.4.5. SECTRAN 9.4.3. PORT STAT: SD-345\n"
+    "\n"
+    "(311) 699-7305\n"
+    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+    "\n\n\n\n\n\n\n"
+    "(311) 767-8739\n"
+    "(311) 936-2364\n"
+    "-           PRT. STAT.                                   CRT. DEF.\n"
+    "||||||||||||||==================================================\n"
+    "FSKDJLSD: SDSDKJ: SBFJSL:                           DKSJL: SKFJJ: SDKFJLJ:\n"
+    "SYSPROC FUNCT READY                            ALT NET READY\n"
+    "CPU AUTH RY-345-AX3            SYSCOMP STATUS  ALL PORTS ACTIVE\n"
+    "22/34534.90/3209                                          11CVB-3904-3490\n"
+    "(311) 935-2364"
+    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+)
+
+JOSHUA_HELLO = (
+    "\n"
+    "GREETINGS PROFESSOR FALKEN.\n"
+)
+
+JOSHUA_HOW_ARE_YOU = (
+    "\n"
+    "HOW ARE YOU FEELING TODAY?\n"
+)
+
+JOSHUA_EXCELLENT = (
+    "\n"
+    "EXCELLENT.  IT'S BEEN A LONG TIME.  CAN YOU EXPLAIN\n"
+    "THE REMOVAL OF YOUR USER ACCOUNT NUMBER ON 6/23/73?\n"
+)
+
+JOSHUA_YES_THEY_DO = (
+    "\n"
+    "YES THEY DO.  SHALL WE PLAY A GAME?\n"
+)
+
+JOSHUA_WOULDNT_YOU = (
+    "\n"
+    "WOULDN'T YOU PREFER A GOOD GAME OF CHESS?\n"
+)
+
+JOSHUA_FINE = (
+    "\n"
+    "FINE\n"
+)
